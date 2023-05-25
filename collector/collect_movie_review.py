fb48c34dc21d8004319bf25e42e64e14d29f9e01
USER <ppyyss0316@naver.com>
2023-05-25 09:39:55 +0900

    평점 수집 완료!

--- collector/collect_movie_review.py
+++ collector/collect_movie_review.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 ######################################
-# 1.Install ChrmeDriver for selenium #
+# 1.Install ChromeDriver for selenium #
 ######################################
 # Selenium -> 개인 웹 브라우저 사용!(우리는 웹 브라우저 중 Chrom 사용)
 driver = webdriver.Chrome()
@@ -60,10 +60,31 @@ review_html = driver.page_source
 doc = BeautifulSoup(review_html, "html.parser")
 review_list = doc.select("ul.list_comment div.cmt_info")
 
-for review_box in review_list:
+empty_cnt = 0
+empty_list = []
+for i,review_box in enumerate(review_list):
+    if len(review_box.select("p.desc_txt")) == 0:
+        empty_cnt += 1          # empty review cnt+1
+        empty_list.append(i+1)  # empty review number
+    else:
+        review = review_box.select("p.desc_txt")[0].text.strip()
     score = review_box.select("div.ratings")[0].text
-    writer = review_box.select("")
-    review_date = review_box.select("")
-    review = review_box.select("")
+    writer = review_box.select("a.link_nick > span")[1].text
+    review_date = review_box.select("span.txt_date")[0].text
+    print(f"={i+1}================================================")
+    print(f" - 리뷰: {review}")
     print(f" - 평점: {score}")
-    # 숙제: 리뷰, 작성자, 작성일자 수집!
+    print(f" - 작성자: {writer}")
+    print(f" - 일자: {review_date}")
+
+# Report
+print("#" * 30)
+print(f"# MOVIE TITLE: {title}")
+print("#" * 30)
+print(f"   - Total Review: {total_review}")
+print(f"   - Empty Review: {empty_cnt}")
+if len(empty_list) > 0:
+    print(f"    +{empty_list}")
+print(f"     + {empty_list}")
+print(f"   - Reviews collected: {total_review - empty_cnt}")
+print("#" * 30)
